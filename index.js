112f93623027d9373d0b1d28cd0be8037814579b
AP8673 <babaabhishek86@gmail.com>
2022-09-04 09:26:50 +0530

    generated para

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const input = document.getElementById("word-length");
+const container = document.getElementById("container");
 
 const generateWord = (n) => {
     let text = "";
@@ -15,7 +16,19 @@ const generateWord = (n) => {
 const generatePara = () => {
     const numOfWords = Number(input.value);
 
-    
-}
+    let data = "";
 
-document.getElementById("generate").onclick = generateWord;
+    for(let i=0; i<numOfWords; ++i){
+        let randomNumber = Math.floor(Math.random()*15);
+        data += generateWord(randomNumber);
+        data += " ";
+    }
+    const para = document.createElement("p");
+    para.innerText = data;
+
+    para.setAttribute("class", "para");
+
+    container.appendChild(para);
+};
+
+document.getElementById("generate").onclick = generatePara;
