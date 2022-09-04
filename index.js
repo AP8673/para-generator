8f2b116e66a69229e32b1b6bf17e13c7893f527c
AP8673 <babaabhishek86@gmail.com>
2022-09-04 08:09:00 +0530

    generated words

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const input = document.getElementById("word-length");
-let numOfWords;
-const getData = () => {
-    numOfWords = Number(input.value);
-    console.log(numOfWords);
+
+const generateWord = (n) => {
+    let text = "";
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for(let i=0; i<n; ++i)
+    {
+        text += letters[Math.floor(Math.random()*25)];
+    }
+
+    return text;
 };
 
-document.getElementById("generate").onclick = getData;
+
+document.getElementById("generate").onclick = generateWord;
